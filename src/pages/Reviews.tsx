4c65f6e9f224a6271f409862a0051f777c77dc64
Arslan Atakhanov <127810063+arslan01000@users.noreply.github.com>
2026-01-16 05:22:15 -0600

    Update Reviews.tsx

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -124,7 +124,8 @@ export default function Reviews() {
   .eq("id", "180868e1-de2b-4257-be36-1143d08fa2a7")
   .single();
 
-console.log("DBG profile single:", dbg);
+console.log("DBG profile single:", JSON.stringify(dbg, null, 2));
+
 
       if (profilesError) throw profilesError;
 
